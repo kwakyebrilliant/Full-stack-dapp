0e9415167cc1db0c0ad3d408062c68ff40f17346
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-07-04 13:43:20 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -8,8 +8,21 @@ const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 function App() {
   return (
     <div className="App">
-      <button>Fetch Greetings</button>
-      <button>Set Greetings</button>
+      <div className='App-header'>
+      <div className="description">
+          <h1>Greeter.sol</h1>
+          <h3>Full stack dapp using ReactJS and Hardhat</h3>
+        </div>
+      <div className='custom-buttons'>
+      <button style={{ backgroundColor: "blue" }}>Fetch Greetings</button>
+      <button style={{ backgroundColor: "green" }}>Set Greetings</button>
+      </div>
+      
+
+      <input 
+      placeholder='Set Greeter Message'
+      />
+      </div>
     </div>
   );
 }
